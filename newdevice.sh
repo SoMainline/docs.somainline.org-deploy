7ea4a2947bd6292b6dab8dfdea5f5db51a237b0f
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-02-20 20:59:26 +0100

    newdevice.sh: Make things nice and case-sensitive

--- newdevice.sh
+++ newdevice.sh
@@ -17,23 +17,23 @@ if ! [ -d "source/platform/$1" ]; then
 	echo "This platform is not supported, creating a directory."
 	mkdir source/platform/$1 && if [ $DEBUG == 1 ]; then echo "Directory created successfully."; fi
 	cp source/template/platform/index.rst source/platform/$1 && if [ $DEBUG == 1 ]; then echo "Template file copied."; fi
-	sed -i s/PLATFORMNAME/$1/g source/platform/$1/index.rst
+	sed -i s/PLATFORMNAME/${1^^}/g source/platform/$1/index.rst
 fi
 
 if ! [ -d "source/platform/$1/$2" ]; then
 	echo "There are no devices made by this vendor with this SoC that are supported yet, craeting a directory."
 	mkdir source/platform/$1/$2 && if [ $DEBUG == 1 ]; then echo "Directory created successfully."; fi
 	cp source/template/platform/vendor/index.rst source/platform/$1/$2/ && if [ $DEBUG == 1 ]; then echo "Template file copied."; fi
-	sed -i s/VENDORNAME/$2/g source/platform/$1/$2/index.rst
-	sed -i s/PLATFORMNAME/$1/g source/platform/$1/$2/index.rst
+	sed -i s/VENDORNAME/${2^}/g source/platform/$1/$2/index.rst
+	sed -i s/PLATFORMNAME/${1^^}/g source/platform/$1/$2/index.rst
 fi
 
 if ! [ -d "source/platform/$1/$2/$3" ]; then
 	echo "Adding support for $2-$3."
 	mkdir source/platform/$1/$2/$3 && if [ $DEBUG == 1 ]; then echo "Directory created successfully."; fi
 	cp source/template/platform/vendor/device/index.rst source/platform/$1/$2/$3 && if [ $DEBUG == 1 ]; then echo "Template file copied."; fi
-	sed -i s/VENDORNAME/$2/g source/platform/$1/$2/$3/index.rst
-	sed -i s/DEVICENAME/$3/g source/platform/$1/$2/$3/index.rst
+	sed -i s/VENDORNAME/${2^}/g source/platform/$1/$2/$3/index.rst
+	sed -i s/DEVICENAME/${3^}/g source/platform/$1/$2/$3/index.rst
 	# We're done boys!
 	echo "$2-$3 added successfully! Now go include some info :)"
 	exit 1
